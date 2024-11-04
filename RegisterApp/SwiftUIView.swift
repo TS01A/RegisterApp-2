bbd43085a92dc12473e4900aa8fe8bf93ca2fd31
AbdullahEesa1 <skl_7@hotmail.com>
2024-11-04 17:09:31 +0300

    Update1

---
+++ RegisterApp/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  RegisterApp
+//
+//  Created by Abdullah on 29/03/1446 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
